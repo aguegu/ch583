6e242effd87e2dd990ee6d24721cd7a8de3192e7
Weihong Guan <weihong.guan@gmail.com>
2024-02-26 19:41:26 +0800

    init imo service

--- examples/ble-iom/Profile/include/iomservice.h
+++ examples/ble-iom/Profile/include/iomservice.h
@@ -5,7 +5,14 @@
 extern "C" {
 #endif
 
+#define IOM_SERV_UUID 0x1815
+
+// Heart Rate Service bit fields
+#define IOM_SERVICE                  0x00000001
+
 // extern void IOM_HandleConnStatusCB(uint16_t connHandle, uint8_t changeType);
+
+extern bStatus_t IOM_AddService(uint32_t services);
 
 #ifdef __cplusplus
 }
