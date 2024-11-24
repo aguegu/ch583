d83edc9941a896501a0be8df2f032c6e1b10d692
aGuegu <weihong.guan@gmail.com>
2024-11-24 11:25:26 +0800

    execute ringbuffer functions in high code

--- extra/examples/crane/main.c
+++ extra/examples/crane/main.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "CH58x_common.h"
-#include "crc16.h"
 #include "ringbuffer.h"
 #include "at.h"
 
@@ -8,25 +7,41 @@
 // UART1: PA8: RXD1; PA9: TXD1
 // UART2: PA6: RXD2; PA7: TXD2
 
+// Keyboard:
+// Up: PB17, Down: PB9, Left: PB16, Right: PB8, Mid: PA3
+// Set: PA1, Rst: PA0
+
+#define KEYBOARD_RIGHT GPIO_Pin_8
+#define KEYBOARD_DOWN GPIO_Pin_9
+#define KEYBOARD_LEFT GPIO_Pin_16
+#define KEYBOARD_UP GPIO_Pin_17
+
+#define KEYBOARD_MID GPIO_Pin_3
+#define KEYBOARD_SET GPIO_Pin_1
+#define KEYBOARD_RST GPIO_Pin_0
+
 #define __bswap_16(x) ((uint16_t) ((((x) >> 8) & 0xff) | (((x) & 0xff) << 8)))
 
-#define LED  GPIO_Pin_19
+#define LED1 GPIO_Pin_18
+#define LED2 GPIO_Pin_19
 
-volatile uint32_t jiffies = 0;
-volatile uint32_t lastReceivedAt = 0;
+typedef void (*TaskFunction)(void);
 
-void delayInJiffy(uint32_t t) {
-  uint32_t start = jiffies;
-  while (t) {
-    if (jiffies != start) {
-      t--;
-      start++;
-    } else {
-      __WFI();
-      __nop();
-      __nop();
-    }
-  }
+typedef struct {
+  TaskFunction task;
+  uint32_t period;
+  uint32_t delay;
+  BOOL ready;
+} Task;
+
+#define TASK_LEN (3)
+
+static Task taskList[TASK_LEN];
+
+void registerTask(uint8_t index, TaskFunction task, unsigned int period, unsigned int delay) {
+  taskList[index].task = task;
+  taskList[index].period = period;
+  taskList[index].delay = delay;
 }
 
 RingBuffer tx0Buffer, rx0Buffer;
@@ -34,21 +49,25 @@ RingBuffer tx1Buffer, rx1Buffer;
 
 int _write(int fd, char *buf, int size) {
   for (int i = 0; i < size; i++) {
-    ringbuffer_put(&tx1Buffer, *buf++, TRUE);
+    ringbufferPut(&tx1Buffer, *buf++, TRUE);
     if (R8_UART1_LSR & RB_LSR_TX_ALL_EMP) {
-      R8_UART1_THR = ringbuffer_get(&tx1Buffer);
+      R8_UART1_THR = ringbufferGet(&tx1Buffer);
     }
   }
   return size;
 }
 
 void flushUart0Tx() {
-  // while (ringbuffer_available(&tx0Buffer));
-  while (!(R8_UART0_LSR & RB_LSR_TX_ALL_EMP));
+  while (!(R8_UART0_LSR & RB_LSR_TX_FIFO_EMP));
 }
 
 void flushUart1Tx() {
-  while (!(R8_UART1_LSR & RB_LSR_TX_ALL_EMP));
+  // while (ringbufferAvailable(&tx1Buffer));
+  while (!(R8_UART1_LSR & RB_LSR_TX_FIFO_EMP)) {
+    __WFI();
+    __nop();
+    __nop();
+  };
 }
 
 void handleAT(uint8_t * payload, uint8_t len) {
@@ -88,25 +107,25 @@ void handleATECHO(uint8_t * payload, uint8_t len) {
 
 void handleATFWD(uint8_t * payload, uint8_t len) {
   for (uint8_t i = 0; i < len; i++) {
-    ringbuffer_put(&tx0Buffer, payload[i], TRUE);
-    if (R8_UART0_LSR & RB_LSR_TX_ALL_EMP) {
-      R8_UART0_THR = ringbuffer_get(&tx0Buffer);
-      GPIOB_ResetBits(LED);
+    ringbufferPut(&tx0Buffer, payload[i], TRUE);
+    if (R8_UART0_LSR & RB_LSR_TX_FIFO_EMP) {
+      R8_UART0_THR = ringbufferGet(&tx0Buffer);
+      GPIOB_ResetBits(LED2);
     }
   }
 
   flushUart0Tx();
-  lastReceivedAt = jiffies;
-  GPIOB_SetBits(LED);
+  // lastReceivedAt = jiffies;
+  GPIOB_SetBits(LED2);
 
-  while (jiffies != (uint32_t)(lastReceivedAt + 2)) {
-    __WFI();
-    __nop();
-    __nop();
-  }
+  // while (jiffies != (uint32_t)(lastReceivedAt + 2)) {
+  //   __WFI();
+  //   __nop();
+  //   __nop();
+  // }
 
-  while (ringbuffer_available(&rx0Buffer)) {
-    printf("%02X", ringbuffer_get(&rx0Buffer));
+  while (ringbufferAvailable(&rx0Buffer)) {
+    printf("%02X", ringbufferGet(&rx0Buffer));
   }
 
   sendOK();
@@ -122,14 +141,14 @@ const static CommandHandler atHandlers[] = {
   { NULL, TRUE, NULL}  // End marker
 };
 
-BOOL athandler() {
+void taskAtCommands() {
   static uint8_t command[256];
   static uint8_t l = 0;
   static uint8_t content[128];
   BOOL LFrecevied = FALSE;
 
-  while (ringbuffer_available(&rx1Buffer)) {
-    uint8_t temp = ringbuffer_get(&rx1Buffer);
+  while (ringbufferAvailable(&rx1Buffer)) {
+    uint8_t temp = ringbufferGet(&rx1Buffer);
     if (temp == '\n') {
       LFrecevied = TRUE;
       break;
@@ -166,21 +185,66 @@ BOOL athandler() {
     l = 0;
     flushUart1Tx();
   }
-  return LFrecevied;
+}
+
+void keyboardInit() {
+  GPIOB_ModeCfg(KEYBOARD_UP, GPIO_ModeIN_PU);
+  GPIOB_ModeCfg(KEYBOARD_DOWN, GPIO_ModeIN_PU);
+  GPIOB_ModeCfg(KEYBOARD_LEFT, GPIO_ModeIN_PU);
+  GPIOB_ModeCfg(KEYBOARD_RIGHT, GPIO_ModeIN_PU);
+
+  GPIOA_ModeCfg(KEYBOARD_MID, GPIO_ModeIN_PU);
+  GPIOA_ModeCfg(KEYBOARD_SET, GPIO_ModeIN_PU);
+  GPIOA_ModeCfg(KEYBOARD_RST, GPIO_ModeIN_PU);
+}
+
+void taskKeyboardPool() {
+  uint32_t directions = GPIOB_ReadPortPin(KEYBOARD_UP | KEYBOARD_DOWN | KEYBOARD_LEFT | KEYBOARD_RIGHT);
+  uint32_t buttons = GPIOA_ReadPortPin(KEYBOARD_MID | KEYBOARD_RST | KEYBOARD_SET);
+
+  // (buttons & KEYBOARD_MID) ? GPIOB_SetBits(LED2) : GPIOB_ResetBits(LED2);
+  (directions & KEYBOARD_RIGHT) ? GPIOB_SetBits(LED2) : GPIOB_ResetBits(LED2);
+}
+
+void dispatchTasks(void) {
+  while (1) {
+    BOOL idle = TRUE;
+    for (uint8_t i = 0; i < TASK_LEN; i++) {
+      if (taskList[i].ready) {
+        taskList[i].ready = FALSE;
+        idle = FALSE;
+        taskList[i].task();
+      }
+    }
+    if (idle) {
+      __WFI();
+      __nop();
+      __nop();
+    }
+  }
+}
+
+void taskBlink(void) {
+  GPIOB_InverseBits(LED1);
 }
 
 int main() {
   SetSysClock(CLK_SOURCE_PLL_60MHz);
-  SysTick_Config(GetSysClock() / 60); // 60Hz
+  SysTick_Config(GetSysClock() / 1800); // 60Hz
 
-  ringbuffer_init(&tx0Buffer, 64);
-  ringbuffer_init(&rx0Buffer, 128);
+  keyboardInit();
 
-  ringbuffer_init(&tx1Buffer, 64);
-  ringbuffer_init(&rx1Buffer, 128);
+  ringbufferInit(&tx0Buffer, 64);
+  ringbufferInit(&rx0Buffer, 128);
 
-  GPIOB_ModeCfg(LED, GPIO_ModeOut_PP_5mA);
-  GPIOB_SetBits(LED);
+  ringbufferInit(&tx1Buffer, 64);
+  ringbufferInit(&rx1Buffer, 128);
+
+  GPIOB_ModeCfg(LED1, GPIO_ModeOut_PP_5mA);
+  GPIOB_SetBits(LED1);
+
+  GPIOB_ModeCfg(LED2, GPIO_ModeOut_PP_5mA);
+  GPIOB_SetBits(LED2);
 
   GPIOB_ResetBits(GPIO_Pin_3);
   GPIOB_ModeCfg(GPIO_Pin_3, GPIO_ModeOut_PP_5mA);
@@ -190,7 +254,7 @@ int main() {
   GPIOB_ModeCfg(GPIO_Pin_5, GPIO_ModeOut_PP_5mA); // DTR: PB5, pushpull
   GPIOB_ModeCfg(GPIO_Pin_4, GPIO_ModeIN_PU);      // RXD: RB4, in with pullup
 
-  UART0_BaudRateCfg(9600);
+  UART0_BaudRateCfg(115200);
 
   R8_UART0_MCR = RB_MCR_HALF | RB_MCR_TNOW;
   R8_UART0_FCR = (3 << 6) | RB_FCR_TX_FIFO_CLR | RB_FCR_RX_FIFO_CLR | RB_FCR_FIFO_EN;
@@ -204,24 +268,32 @@ int main() {
   GPIOA_SetBits(GPIO_Pin_9);
   GPIOA_ModeCfg(GPIO_Pin_9, GPIO_ModeOut_PP_5mA); // TXD: PA9, pushpull, but set it high beforehand
   GPIOA_ModeCfg(GPIO_Pin_8, GPIO_ModeIN_PU);      // RXD: PA8, in with pullup
+
   UART1_DefInit();  // default baudrate 115200
   UART1_ByteTrigCfg(UART_7BYTE_TRIG);
   UART1_INTCfg(ENABLE, RB_IER_THR_EMPTY | RB_IER_RECV_RDY);
   PFIC_EnableIRQ(UART1_IRQn);
 
-  while(1) {
-    if (!athandler()) {
-      __WFI();
-      __nop();
-      __nop();
-    }
-  }
+  registerTask(0, taskBlink, 180, 1);
+  registerTask(1, taskAtCommands, 12, 0);
+  registerTask(2, taskKeyboardPool, 120, 2);  // 1800 / 120 = 15 Hz
+
+  dispatchTasks();
 }
 
 __INTERRUPT
 __HIGH_CODE
 void SysTick_Handler(void) {
-  jiffies++;
+  static volatile uint8_t i;
+  for (i = 0; i < TASK_LEN; i++) {
+    if (taskList[i].delay == 0) {
+      taskList[i].ready = TRUE;
+      taskList[i].delay = taskList[i].period - 1;
+    } else {
+      taskList[i].delay--;
+    }
+  }
+
   SysTick->SR = 0;
 }
 
@@ -230,15 +302,15 @@ __HIGH_CODE
 void UART0_IRQHandler(void) {
   switch (UART0_GetITFlag()) {
     case UART_II_THR_EMPTY: // trigger when THR and FIFOtx all empty
-      while (ringbuffer_available(&tx0Buffer) && R8_UART0_TFC < UART_FIFO_SIZE) {
-        R8_UART0_THR = ringbuffer_get(&tx0Buffer);
+      while (ringbufferAvailable(&tx0Buffer) && R8_UART0_TFC < UART_FIFO_SIZE) {
+        R8_UART0_THR = ringbufferGet(&tx0Buffer);
       }
       break;
     case UART_II_RECV_RDY: // Rx FIFO is full
     case UART_II_RECV_TOUT: // Rx FIFO is not full, but there is something when no new data comming in within timeout
       while (R8_UART0_RFC) {
-        ringbuffer_put(&rx0Buffer, R8_UART0_RBR, FALSE);
-        lastReceivedAt = jiffies;
+        ringbufferPut(&rx0Buffer, R8_UART0_RBR, FALSE);
+        // lastReceivedAt = jiffies;
       }
       break;
   }
@@ -249,14 +321,14 @@ __HIGH_CODE
 void UART1_IRQHandler(void) {
   switch (UART1_GetITFlag()) {
     case UART_II_THR_EMPTY: // trigger when THR and FIFOtx all empty
-      while (ringbuffer_available(&tx1Buffer) && R8_UART1_TFC < UART_FIFO_SIZE) {
-        R8_UART1_THR = ringbuffer_get(&tx1Buffer);
+      while (ringbufferAvailable(&tx1Buffer) && R8_UART1_TFC < UART_FIFO_SIZE) {
+        R8_UART1_THR = ringbufferGet(&tx1Buffer);
       }
       break;
     case UART_II_RECV_RDY: // Rx FIFO is full
     case UART_II_RECV_TOUT: // Rx FIFO is not full, but there is something when no new data comming in within timeout
       while (R8_UART1_RFC) {
-        ringbuffer_put(&rx1Buffer, R8_UART1_RBR, FALSE);
+        ringbufferPut(&rx1Buffer, R8_UART1_RBR, FALSE);
       }
       break;
   }
--- extra/examples/crane/ringbuffer.c
+++ extra/examples/crane/ringbuffer.c
@@ -1,29 +1,31 @@
 #include "ringbuffer.h"
 
 
-void ringbuffer_init(RingBuffer * rb, uint16_t size) {
+void ringbufferInit(RingBuffer * rb, uint16_t size) {
   rb->buffer = (uint8_t *)malloc(size);
   rb->indexIn = 0;
   rb->indexOut = 0;
   rb->mask = size - 1;
 }
 
-void ringbuffer_put(RingBuffer * rb, uint8_t c, BOOL waitForConsuming) {
+__HIGH_CODE void ringbufferPut(RingBuffer * rb, uint8_t c, BOOL waitForConsuming) {
   uint8_t indexInNext = (rb->indexIn + 1) & rb->mask;
   while (waitForConsuming && indexInNext == rb->indexOut) {
     __WFI();
+    __nop();
+    __nop();
   };
   rb->buffer[rb->indexIn] = c;
   rb->indexIn = indexInNext;
 }
 
-inline uint8_t ringbuffer_get(RingBuffer * rb) {
+__HIGH_CODE uint8_t ringbufferGet(RingBuffer * rb) {
   uint8_t indexOutNext = (rb->indexOut + 1) & rb->mask;
   uint8_t c = rb->buffer[rb->indexOut];
   rb->indexOut = indexOutNext;
   return c;
 }
 
-BOOL ringbuffer_available(RingBuffer *rb) {
+__HIGH_CODE BOOL ringbufferAvailable(RingBuffer *rb) {
   return rb->indexIn != rb->indexOut;
 }
